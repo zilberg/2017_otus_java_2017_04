141bd678e11575ce46dbdd46add2f805736ee692
zilberg <zilberg15@yandex.ru>
2017-04-02 18:58:45 +0300

    Hello worls

---
+++ hometask1/src/main/java/ru/zilberg/hometask1/Main.java
@@ -0,0 +1,10 @@
+package ru.zilberg.hometask1;
+
+/**
+ * Created by zilberg15 on 02.04.17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("My firs hometask Otus Java 2017 04");
+    }
+}
