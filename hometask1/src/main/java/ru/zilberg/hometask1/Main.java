395469e1df6aae663ac0bebd734a381d4afb25c3
zilberg <zilberg15@yandex.ru>
2017-04-11 07:12:56 +0300

    The fix hometask1

--- hometask1/src/main/java/ru/zilberg/hometask1/Main.java
+++ hometask1/src/main/java/ru/zilberg/hometask1/Main.java
@@ -1,10 +1,42 @@
 package ru.zilberg.hometask1;
 
-/**
- * Created by zilberg15 on 02.04.17.
- */
+import au.com.bytecode.opencsv.CSVReader;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("My firs hometask Otus Java 2017 04");
+    public static void main(String[] args) throws IOException {
+        //System.out.println("My firs hometask Otus Java 2017 04");
+
+        String pathToFile;
+
+        if (args.length >= 1){
+            pathToFile = args[0];
+        }else {
+            pathToFile = "emails.csv";
+        }
+
+        System.out.println("Reading mails from: " + pathToFile);
+
+        CSVReader reader = new CSVReader(new FileReader(pathToFile));
+
+        List <String> emails = reader.readAll().stream().map(line -> line[0].trim()).collect(Collectors.toList());
+
+        System.out.println("Emails count:" + emails.size());
+
+        emails.forEach(System.out::println);
+
     }
+
+
+
+
+
+
+
+
+
 }
